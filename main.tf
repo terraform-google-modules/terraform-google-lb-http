25ac363d4be0622997d7e29a4ad687101b7ee232
Peter Green <peter.green@aztek-native.com>
2018-05-09 23:28:42 +0100

    Configure the health-check interval and timeout.

--- main.tf
+++ main.tf
@@ -90,11 +90,13 @@ resource "google_compute_backend_service" "default" {
 }
 
 resource "google_compute_http_health_check" "default" {
-  project      = "${var.project}"
-  count        = "${length(var.backend_params)}"
-  name         = "${var.name}-backend-${count.index}"
-  request_path = "${element(split(",", element(var.backend_params, count.index)), 0)}"
-  port         = "${element(split(",", element(var.backend_params, count.index)), 2)}"
+  project            = "${var.project}"
+  count              = "${length(var.backend_params)}"
+  name               = "${var.name}-backend-${count.index}"
+  request_path       = "${element(split(",", element(var.backend_params, count.index)), 0)}"
+  port               = "${element(split(",", element(var.backend_params, count.index)), 2)}"
+  timeout_sec        = "${element(split(",", element(var.backend_params, count.index)), 4)}"
+  check_interval_sec = "${element(split(",", element(var.backend_params, count.index)), 3)}"
 }
 
 # Create firewall rule for each backend in each network specified, uses mod behavior of element().
