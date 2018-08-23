d051befc7398809b179700c2a2e15b1464801f24
Dan Isla <disla@google.com>
2018-08-23 17:16:54 +0000

    [tests] added https-gke to pull-requests pipeline

--- tests/set_pipelines.sh
+++ tests/set_pipelines.sh
@@ -3,9 +3,11 @@
 fly -t tf set-pipeline -p tf-examples-lb-http-basic -c tests/pipelines/tf-examples-lb-http-basic.yaml -l tests/pipelines/values.yaml
 fly -t tf set-pipeline -p tf-examples-lb-http-nat-gw -c tests/pipelines/tf-examples-lb-http-nat-gw.yaml -l tests/pipelines/values.yaml
 fly -t tf set-pipeline -p tf-examples-lb-https-content -c tests/pipelines/tf-examples-lb-https-content.yaml -l tests/pipelines/values.yaml
+fly -t tf set-pipeline -p tf-examples-lb-https-gke -c tests/pipelines/tf-examples-lb-https-gke.yaml -l tests/pipelines/values.yaml
 fly -t tf set-pipeline -p tf-lb-http-pull-requests -c tests/pipelines/tf-lb-http-pull-requests.yaml -l tests/pipelines/values.yaml
 
 fly -t tf expose-pipeline -p tf-examples-lb-http-basic
 fly -t tf expose-pipeline -p tf-examples-lb-http-nat-gw
 fly -t tf expose-pipeline -p tf-examples-lb-https-content
+fly -t tf expose-pipeline -p tf-examples-lb-https-gke
 fly -t tf expose-pipeline -p tf-lb-http-pull-requests
