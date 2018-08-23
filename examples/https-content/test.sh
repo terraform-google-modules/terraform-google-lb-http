11d602c2d506bbefc560138d5c1bce1221439e85
Dan Isla <disla@google.com>
2018-08-23 18:06:52 +0000

    [tests] increase load balancer wait time to 1 hour...

--- examples/https-content/test.sh
+++ examples/https-content/test.sh
@@ -6,13 +6,13 @@ set -e
 URL="https://$(terraform output load-balancer-ip)"
 status=0
 count=0
-while [[ $count -lt 240 && $status -ne 200 ]]; do
+while [[ $count -lt 720 && $status -ne 200 ]]; do
   echo "INFO: Waiting for load balancer..."
   status=$(curl -sfk -m 5 -o /dev/null -w "%{http_code}" "${URL}" || true)
   ((count=count+1))
   sleep 5
 done
-if [[ $count -lt 240 ]]; then
+if [[ $count -lt 720 ]]; then
   echo "INFO: PASS. Load balancer is ready."
 else
   echo "ERROR: Failed. Load balancer never became ready."
