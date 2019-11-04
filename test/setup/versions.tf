11c316ead0385b30bf1207ede6e479250bef2ede
Devan Patel <devan2patel@gmail.com>
2019-11-04 15:00:43 +0000

    test: bump versions

--- test/setup/versions.tf
+++ test/setup/versions.tf
@@ -15,13 +15,13 @@
  */
 
 terraform {
-  required_version = ">= 0.12"
+  required_version = "~> 0.12.6"
 }
 
 provider "google" {
-  version = "~> 2.13.0"
+  version = "~> 2.15"
 }
 
 provider "google-beta" {
-  version = "~> 2.13.0"
+  version = "~> 2.15"
 }
