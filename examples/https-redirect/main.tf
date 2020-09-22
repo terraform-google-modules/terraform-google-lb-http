656931b7520c59f790ea2f4112fea925bbd27687
Felipe Martinez <femrtnz@gmail.com>
2020-09-22 18:00:41 +0100

    Adding iap_config missing

--- examples/https-redirect/main.tf
+++ examples/https-redirect/main.tf
@@ -145,6 +145,11 @@ module "gce-lb-http" {
           max_utilization              = null
         }
       ]
+      iap_config = {
+        enable               = false
+        oauth2_client_id     = ""
+        oauth2_client_secret = ""
+      }
     }
   }
 
