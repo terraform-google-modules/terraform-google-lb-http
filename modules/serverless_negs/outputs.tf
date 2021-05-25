386bc8959c85e8cd06d2c3e49ffc8671763ead5e
Benjamin Komen <benjaminkomen@users.noreply.github.com>
2021-05-25 17:47:31 +0200

    fix: Mark output derived from possible sensitive attribute as sensitive (#167)

--- modules/serverless_negs/outputs.tf
+++ modules/serverless_negs/outputs.tf
@@ -17,6 +17,7 @@
 output "backend_services" {
   description = "The backend service resources."
   value       = google_compute_backend_service.default
+  sensitive   = true // can contain sensitive iap_config
 }
 
 output "external_ip" {
