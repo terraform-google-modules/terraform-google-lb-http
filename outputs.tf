5506e2807686d852b584aad30c1a83b08538143c
37238646+denissimonovski@users.noreply.github.com <denis.simonovski@rackspace.com>
2021-08-23 18:28:02 +0200

    run make build

--- outputs.tf
+++ outputs.tf
@@ -44,8 +44,3 @@ output "https_proxy" {
   description = "The HTTPS proxy used by this module."
   value       = google_compute_target_https_proxy.default[*].self_link
 }
-
-output "url_map" {
-  description = "The default URL map used by this module."
-  value = google_compute_url_map.default[*].self_link
-}
