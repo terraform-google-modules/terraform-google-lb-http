8b64f328018545647ba35fb71435cb592e8c08aa
wapfel <94196524+wapfel@users.noreply.github.com>
2021-12-10 14:49:11 -0600

    Update main.tf

--- examples/static-website-with-no-domain/main.tf
+++ examples/static-website-with-no-domain/main.tf
@@ -66,14 +66,14 @@ module "load-balancer" {
 
 }
 
-resource "google_storage_bucket_object" "index.html" {
+resource "google_storage_bucket_object" "index" {
   name         = "index.html"
   content      = file("index.html")
   content_type = "text/html;charset=utf-8"
   bucket       = module.website-storage-buckets[0].name
 }
 
-resource "google_storage_bucket_object" "404.html" {
+resource "google_storage_bucket_object" "404" {
   name         = "404.html"
   content      = file("404.html")
   content_type = "text/html;charset=utf-8"
