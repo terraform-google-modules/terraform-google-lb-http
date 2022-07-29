ff085f167ff83df887bbcbe5f10ae8a64b6da7c9
Andrew Peabody <andrewpeabody@google.com>
2022-07-29 18:20:04 +0000

    chore: add label to Cloud Run Example

--- examples/cloudrun/main.tf
+++ examples/cloudrun/main.tf
@@ -25,13 +25,14 @@ provider "google-beta" {
 # [START cloudloadbalancing_ext_http_cloudrun]
 module "lb-http" {
   source  = "GoogleCloudPlatform/lb-http/google//modules/serverless_negs"
-  version = "~> 6.2.0"
+  version = "~> 6.3"
   name    = var.lb_name
   project = var.project_id
 
   ssl                             = var.ssl
   managed_ssl_certificate_domains = [var.domain]
   https_redirect                  = var.ssl
+  labels                          = { "Example Label" = "Cloud Run Example" }
 
   backends = {
     default = {
