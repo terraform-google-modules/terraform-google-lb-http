319f4165548188dfdf15830c0ddccda339ccf08e
Imran Nayer <29933900+imrannayer@users.noreply.github.com>
2023-01-06 13:34:14 -0600

    feat: added certificate manager support (#294)

--- modules/serverless_negs/main.tf
+++ modules/serverless_negs/main.tf
@@ -22,6 +22,7 @@ locals {
   url_map             = var.create_url_map ? join("", google_compute_url_map.default.*.self_link) : var.url_map
   create_http_forward = var.http_forward || var.https_redirect
 
+
 }
 
 ### IPv4 block ###
@@ -110,6 +111,7 @@ resource "google_compute_target_https_proxy" "default" {
   url_map = local.url_map
 
   ssl_certificates = compact(concat(var.ssl_certificates, google_compute_ssl_certificate.default.*.self_link, google_compute_managed_ssl_certificate.default.*.self_link, ), )
+  certificate_map  = var.certificate_map != null ? "//certificatemanager.googleapis.com/${var.certificate_map}" : null
   ssl_policy       = var.ssl_policy
   quic_override    = var.quic ? "ENABLE" : null
 }
