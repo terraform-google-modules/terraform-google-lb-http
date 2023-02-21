d494be6478433f7e891d5e77cde82e103616c8c2
Grant Sorbo <gtsorbo@google.com>
2023-02-21 20:34:57 +0000

    fix: bump provider requirement

--- versions.tf
+++ versions.tf
@@ -20,11 +20,11 @@ terraform {
 
     google = {
       source  = "hashicorp/google"
-      version = ">= 3.53, < 5.0"
+      version = ">= 4.45, < 5.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 4.40, < 5.0"
+      version = ">= 4.45, < 5.0"
     }
     random = {
       source  = "hashicorp/random"
