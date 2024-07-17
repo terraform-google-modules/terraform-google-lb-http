ae65328b1c2b7a89e14f54871888e5210d8d3c69
Aayush Gautam <aayushgautam@google.com>
2024-07-17 09:55:03 +0000

    Add google_compute_region_network_endpoint_group within terraform blueprint submodule serverless-negs

---
+++ examples/multiple-cloudrun/README.md
@@ -0,0 +1,111 @@
+# HTTPS load balancer with Serverless NEG backend example (Cloud Run)
+
+[![button](http://gstatic.com/cloudssh/images/open-btn.png)](https://console.cloud.google.com/cloudshell/open?git_repo=https://github.com/GoogleCloudPlatform/terraform-google-lb-http&working_dir=examples/cloudrun&page=shell&tutorial=README.md)
+
+This example deploys a Cloud Run application, creates a Serverless Network
+Endpoint Group (NEG) and exposes it behind a Cloud HTTPS load balancer with
+HTTP-to-HTTPS redirection.
+
+You can tweak this example to enable other functionalities such as:
+
+- serving static assets from Cloud CDN
+- enabling a security profile via Cloud Armor
+- run global endpoints by deploying Cloud Run service to multiple regions.
+
+## Change to the example directory
+
+```
+[[ `basename $PWD` != cloudrun ]] && cd examples/cloudrun
+```
+
+## Install Terraform
+
+1. Install Terraform if it is not already installed (visit
+   [terraform.io](https://terraform.io) for other distributions):
+
+## Set up the environment
+
+1. Set the project, replace `YOUR_PROJECT` with your project ID:-
+
+```
+PROJECT=YOUR_PROJECT
+```
+
+```
+gcloud config set project ${PROJECT}
+```
+
+2. Configure the environment for Terraform:
+
+```
+[[ $CLOUD_SHELL ]] || gcloud auth application-default login
+export GOOGLE_PROJECT=$(gcloud config get-value project)
+```
+
+## Option 1: Run on HTTP load balancer (unencrypted, not recommended)
+
+This option provisions an HTTP forwarding rule (insecure) and is not recommended
+for production use. It is provided since it provisions faster than the Option 2.
+
+1. Initialize:
+
+    ```
+    terraform init
+    ```
+
+1. Deploy the load balancer, replace `example.com` with your domain name.
+
+    ```
+    terraform apply -var=project_id=$PROJECT \
+        -var=ssl=false -var=domain=null
+    ```
+
+1. It may take some time for the load balancer to provision. Visit the output
+   IP address of the load balancer.
+
+## Option 2: Run on HTTPS load balancer (with HTTP-to-HTTPS redirect)
+
+This options creates a Google-managed SSL certificate for your domain name,
+sets it up on HTTPS forwarding rule and creates a HTTP forwarding rule to
+redirect HTTP traffic to HTTPS.
+
+1. Make sure you have a **domain name**. This is required since we provision a
+   Google-managed SSL certificate specifically for this domain name.
+
+1. Initialize:
+
+    ```
+    terraform init
+    ```
+
+1. Deploy the load balancer, replace `example.com` with your domain name.
+
+    ```
+    terraform apply -var=project_id=$PROJECT \
+        -var=domain=example.com
+    ```
+
+1. After the deployment completes it outputs the IP address of the load balancer.
+   Update DNS records for your domain to point to this IP address.
+
+1. It may take around half an hour for the SSL certificate to be provisioned
+   and the application to start serving traffic.
+
+<!-- BEGINNING OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| domain | Domain name to run the load balancer on. Used if `ssl` is `true`. | `string` | n/a | yes |
+| lb\_name | Name for load balancer and associated resources | `string` | `"tf-cr-lb"` | no |
+| project\_id | n/a | `string` | n/a | yes |
+| region | Location for load balancer and Cloud Run resources | `string` | `"us-central1"` | no |
+| ssl | Run load balancer on HTTPS and provision managed certificate with provided `domain`. | `bool` | `true` | no |
+
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| load-balancer-ip | n/a |
+
+<!-- END OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
---
+++ modules/serverless_negs_with_endpoint/variables.tf
@@ -0,0 +1,290 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+variable "project" {
+  description = "The project to deploy to, if not set the default provider project is used."
+  type        = string
+}
+
+variable "name" {
+  description = "Name for the forwarding rule and prefix for supporting resources"
+  type        = string
+}
+
+variable "create_address" {
+  type        = bool
+  description = "Create a new global IPv4 address"
+  default     = true
+}
+
+variable "address" {
+  type        = string
+  description = "Existing IPv4 address to use (the actual IP address value)"
+  default     = null
+}
+
+variable "enable_ipv6" {
+  type        = bool
+  description = "Enable IPv6 address on the CDN load-balancer"
+  default     = false
+}
+
+variable "create_ipv6_address" {
+  type        = bool
+  description = "Allocate a new IPv6 address. Conflicts with \"ipv6_address\" - if both specified, \"create_ipv6_address\" takes precedence."
+  default     = false
+}
+
+variable "ipv6_address" {
+  type        = string
+  description = "An existing IPv6 address to use (the actual IP address value)"
+  default     = null
+}
+
+
+variable "backends" {
+  description = "Map backend indices to list of backend maps."
+  type = map(object({
+    project                 = optional(string)
+    protocol                = optional(string)
+    port_name               = optional(string)
+    description             = optional(string)
+    enable_cdn              = optional(bool)
+    compression_mode        = optional(string)
+    security_policy         = optional(string, null)
+    edge_security_policy    = optional(string, null)
+    custom_request_headers  = optional(list(string))
+    custom_response_headers = optional(list(string))
+
+    connection_draining_timeout_sec = optional(number)
+    session_affinity                = optional(string)
+    affinity_cookie_ttl_sec         = optional(number)
+    locality_lb_policy              = optional(string)
+
+
+    log_config = object({
+      enable      = optional(bool)
+      sample_rate = optional(number)
+    })
+
+    groups = list(object({
+      group       = string
+      description = optional(string)
+
+    }))
+    iap_config = object({
+      enable               = bool
+      oauth2_client_id     = optional(string)
+      oauth2_client_secret = optional(string)
+    })
+    cdn_policy = optional(object({
+      cache_mode                   = optional(string)
+      signed_url_cache_max_age_sec = optional(string)
+      default_ttl                  = optional(number)
+      max_ttl                      = optional(number)
+      client_ttl                   = optional(number)
+      negative_caching             = optional(bool)
+      negative_caching_policy = optional(object({
+        code = optional(number)
+        ttl  = optional(number)
+      }))
+      serve_while_stale = optional(number)
+      cache_key_policy = optional(object({
+        include_host           = optional(bool)
+        include_protocol       = optional(bool)
+        include_query_string   = optional(bool)
+        query_string_blacklist = optional(list(string))
+        query_string_whitelist = optional(list(string))
+        include_http_headers   = optional(list(string))
+        include_named_cookies  = optional(list(string))
+      }))
+      bypass_cache_on_request_headers = optional(list(string))
+    }))
+    outlier_detection = optional(object({
+      base_ejection_time = optional(object({
+        seconds = number
+        nanos   = optional(number)
+      }))
+      consecutive_errors                    = optional(number)
+      consecutive_gateway_failure           = optional(number)
+      enforcing_consecutive_errors          = optional(number)
+      enforcing_consecutive_gateway_failure = optional(number)
+      enforcing_success_rate                = optional(number)
+      interval = optional(object({
+        seconds = number
+        nanos   = optional(number)
+      }))
+      max_ejection_percent        = optional(number)
+      success_rate_minimum_hosts  = optional(number)
+      success_rate_request_volume = optional(number)
+      success_rate_stdev_factor   = optional(number)
+    }))
+  }))
+}
+
+variable "create_url_map" {
+  description = "Set to `false` if url_map variable is provided."
+  type        = bool
+  default     = true
+}
+
+variable "url_map" {
+  description = "The url_map resource to use. Default is to send all traffic to first backend."
+  type        = string
+  default     = null
+}
+
+variable "http_forward" {
+  description = "Set to `false` to disable HTTP port 80 forward"
+  type        = bool
+  default     = true
+}
+
+variable "ssl" {
+  description = "Set to `true` to enable SSL support. If `true` then at least one of these are required: 1) `ssl_certificates` OR 2) `create_ssl_certificate` set to `true` and `private_key/certificate` OR  3) `managed_ssl_certificate_domains`, OR 4) `certificate_map`"
+  type        = bool
+  default     = false
+}
+
+variable "create_ssl_certificate" {
+  description = "If `true`, Create certificate using `private_key/certificate`"
+  type        = bool
+  default     = false
+}
+
+variable "ssl_certificates" {
+  description = "SSL cert self_link list. Requires `ssl` to be set to `true`"
+  type        = list(string)
+  default     = []
+}
+
+variable "private_key" {
+  description = "Content of the private SSL key. Requires `ssl` to be set to `true` and `create_ssl_certificate` set to `true`"
+  type        = string
+  default     = null
+}
+
+variable "certificate" {
+  description = "Content of the SSL certificate. Requires `ssl` to be set to `true` and `create_ssl_certificate` set to `true`"
+  type        = string
+  default     = null
+}
+
+variable "managed_ssl_certificate_domains" {
+  description = "Create Google-managed SSL certificates for specified domains. Requires `ssl` to be set to `true`"
+  type        = list(string)
+  default     = []
+}
+
+variable "certificate_map" {
+  description = "Certificate Map ID in format projects/{project}/locations/global/certificateMaps/{name}. Identifies a certificate map associated with the given target proxy.  Requires `ssl` to be set to `true`"
+  type        = string
+  default     = null
+}
+
+variable "ssl_policy" {
+  type        = string
+  description = "Selfink to SSL Policy"
+  default     = null
+}
+
+variable "quic" {
+  type        = bool
+  description = "Specifies the QUIC override policy for this resource. Set true to enable HTTP/3 and Google QUIC support, false to disable both. Defaults to null which enables support for HTTP/3 only."
+  default     = null
+}
+
+variable "edge_security_policy" {
+  description = "The resource URL for the edge security policy to associate with the backend service"
+  type        = string
+  default     = null
+}
+
+variable "security_policy" {
+  description = "The resource URL for the security policy to associate with the backend service"
+  type        = string
+  default     = null
+}
+
+variable "https_redirect" {
+  description = "Set to `true` to enable https redirect on the lb."
+  type        = bool
+  default     = false
+}
+
+variable "random_certificate_suffix" {
+  description = "Bool to enable/disable random certificate name generation. Set and keep this to true if you need to change the SSL cert."
+  type        = bool
+  default     = false
+}
+
+variable "labels" {
+  description = "The labels to attach to resources created by this module"
+  type        = map(string)
+  default     = {}
+}
+
+variable "load_balancing_scheme" {
+  description = "Load balancing scheme type (EXTERNAL for classic external load balancer, EXTERNAL_MANAGED for Envoy-based load balancer, and INTERNAL_SELF_MANAGED for traffic director)"
+  type        = string
+  default     = "EXTERNAL"
+}
+
+variable "network" {
+  description = "Network for INTERNAL_SELF_MANAGED load balancing scheme"
+  type        = string
+  default     = "default"
+}
+
+variable "server_tls_policy" {
+  description = "The resource URL for the server TLS policy to associate with the https proxy service"
+  type        = string
+  default     = null
+}
+
+variable "http_port" {
+  description = "The port for the HTTP load balancer"
+  type        = number
+  default     = 80
+  validation {
+    condition     = var.http_port >= 1 && var.http_port <= 65535
+    error_message = "You must specify exactly one port between 1 and 65535"
+  }
+}
+
+variable "https_port" {
+  description = "The port for the HTTPS load balancer"
+  type        = number
+  default     = 443
+  validation {
+    condition     = var.https_port >= 1 && var.https_port <= 65535
+    error_message = "You must specify exactly one port between 1 and 65535"
+  }
+}
+
+variable "http_keep_alive_timeout_sec" {
+  description = "Specifies how long to keep a connection open, after completing a response, while there is no matching traffic (in seconds)."
+  type        = number
+  default     = null
+}
+
+variable "cloud_run_service_names"{
+  type = list(string)
+}
+
+variable "region"{
+  type = string
+}
