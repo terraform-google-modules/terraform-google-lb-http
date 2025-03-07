ad2f3fe288f6a5f176d4e239bd166f3675ce4f03
pawan1210 <pawansaggu007@gmail.com>
2025-03-07 07:15:47 +0000

    fix: added default values for variables as per readme

--- examples/internal-lb-cloud-run/variables.tf
+++ examples/internal-lb-cloud-run/variables.tf
@@ -20,18 +20,22 @@ variable "project_id" {
 
 variable "backend_region_a" {
   type = string
+  default = "us-east1"
 }
 
 variable "backend_region_b" {
   type = string
+  default = "us-south1"
 }
 
 variable "subnet_region_a" {
   type = string
+  default = "us-central1"
 }
 
 variable "subnet_region_b" {
   type = string
+  default = "us-west1"
 }
 
 
