295757f512831d75f0b0f0660c6c5cf9dfdd72b2
Nick Whitehill <nWhitehill@users.noreply.github.com>
2025-06-26 20:27:30 +0000

    fix: fix missing https in cert map

--- modules/dynamic_backends/main.tf
+++ modules/dynamic_backends/main.tf
@@ -118,7 +118,7 @@ resource "google_compute_target_https_proxy" "default" {
   url_map = local.url_map
 
   ssl_certificates            = compact(concat(var.ssl_certificates, google_compute_ssl_certificate.default[*].self_link, google_compute_managed_ssl_certificate.default[*].self_link, ), )
-  certificate_map             = var.certificate_map != null ? "//certificatemanager.googleapis.com/${var.certificate_map}" : null
+  certificate_map             = var.certificate_map != null ? "https://certificatemanager.googleapis.com/${var.certificate_map}" : null
   ssl_policy                  = var.ssl_policy
   quic_override               = var.quic == null ? "NONE" : var.quic ? "ENABLE" : "DISABLE"
   server_tls_policy           = var.server_tls_policy
