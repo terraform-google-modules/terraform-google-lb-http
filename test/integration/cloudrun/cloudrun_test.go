b91eddd82de3f65cfb383b54f862c0f4a7d5dd9f
Martijn van Schaardenburg <martijnvs@google.com>
2025-10-15 16:21:51 +0000

    chore: Migrate kitchen -> cft

---
+++ test/integration/cloudrun/cloudrun_test.go
@@ -0,0 +1,42 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cloudrun_test
+
+import (
+	"testing"
+
+	"github.com/GoogleCloudPlatform/cloud-foundation-toolkit/infra/blueprint-test/pkg/tft"
+	"github.com/stretchr/testify/assert"
+
+	test "github.com/terraform-google-modules/terraform-google-lb-http/test/integration"
+)
+
+func TestCloudRunExplicit(t *testing.T) {
+	vars := map[string]interface{}{
+		"ssl":    false,
+		"domain": "asdf",
+	}
+	bpt := tft.NewTFBlueprintTest(t, tft.WithVars(vars))
+
+	bpt.DefineVerify(func(assert *assert.Assertions) {
+		bpt.DefaultVerify(assert)
+
+		loadBalancerIp := bpt.GetStringOutput("load-balancer-ip")
+
+		test.AssertResponseStatus(t, assert, "http://"+loadBalancerIp, 200)
+	})
+
+	bpt.Test()
+}
---
+++ test/integration/https-redirect/https_redirect_test.go
@@ -0,0 +1,50 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package https_redirect_test
+
+import (
+	"crypto/tls"
+	"net/http"
+	"testing"
+
+	"github.com/GoogleCloudPlatform/cloud-foundation-toolkit/infra/blueprint-test/pkg/tft"
+	"github.com/stretchr/testify/assert"
+
+	test "github.com/terraform-google-modules/terraform-google-lb-http/test/integration"
+)
+
+func TestHttpsRedirect(t *testing.T) {
+	bpt := tft.NewTFBlueprintTest(t)
+
+	vars := map[string]interface{}{
+		"network_name": "ci-lb-https-redirect",
+		"project":      bpt.GetTFSetupStringOutput("project_id"),
+	}
+
+	tft.WithVars(vars)(bpt)
+
+	bpt.DefineVerify(func(assert *assert.Assertions) {
+		bpt.DefaultVerify(assert)
+
+		loadBalancerIp := bpt.GetStringOutput("load-balancer-ip")
+
+		test.AssertResponseStatus(t, assert, "http://"+loadBalancerIp, 301)
+
+		insecureClient := &http.Client{Transport: &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}}
+		test.AssertResponseStatusWithClient(t, assert, insecureClient, "https://"+loadBalancerIp, 200)
+	})
+
+	bpt.Test()
+}
